d45fa8c33c10e7afbc0f2d9f65a64146279efaa2
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-09-13 20:28:26 +0200

    project added

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,5 +1,76 @@
 package entities;
+import java.util.*;
+import java.time.LocalDate;
 
 public class Product {
 
+
+    private  String name;
+    private String productId;
+    private int rating;
+
+    private ProductCategory category;
+
+    private LocalDate dateCreated;
+
+    private LocalDate dateModified;
+
+    public Product(String productId, String name, int rating, ProductCategory category, LocalDate dateCreated, LocalDate dateModified){
+        this.productId = productId;
+        this.name = name;
+        this.rating = rating;
+        this.category = category;
+        this.dateCreated = LocalDate.now();
+        this.dateModified = LocalDate.now();
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getRating() {
+        return rating;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+    public LocalDate getDateModified() {
+        return dateModified;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", productId='" + productId + '\'' +
+                ", rating=" + rating +
+                ", category=" + category +
+                ", dateCreated=" + dateCreated +
+                ", dateModified=" + dateModified +
+                '}';
+    }
+
+
 }
+
+
--- src/main/java/service/Warehouse.java
+++ src/main/java/service/Warehouse.java
@@ -1,4 +1,15 @@
 package service;
 
+import java.time.LocalDate;
+import java.util.*;
+import entities.Product;
+import entities.ProductCategory;
+
 public class Warehouse {
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("12","phone",5, ProductCategory.ELECTRONICS, LocalDate.now(),LocalDate.now()));
+
+        System.out.println("products: " + products);
+    }
 }
