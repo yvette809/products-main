91644590c46de39d8b79aa7de6765176a211aa32
yvette809 <60625477+yvette809@users.noreply.github.com>
2023-09-19 17:37:52 +0200

    test added for getProductsById and getProductsByCategory

--- src/test/java/org/example/WarehouseTest.java
+++ src/test/java/org/example/WarehouseTest.java
@@ -2,27 +2,43 @@ package org.example;
 
 import entities.Product;
 import entities.ProductCategory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import service.Warehouse;
-
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class WarehouseTest {
-    Warehouse warehouse = new Warehouse();
+    private Warehouse warehouse;
+    @BeforeEach
+            public void setUp(){
+         warehouse = new Warehouse();
+        Product product1 = new Product("laptop",6,ProductCategory.ELECTRONICS);
+        Product product2 = new Product("Ball",7,ProductCategory.OTHER);
+        Product product3 = new Product("dog",5,ProductCategory.ANIMALS);
+        Product product4 = new Product("smart phone",4,ProductCategory.ELECTRONICS);
+        Product product5 = new Product("Cat",8,ProductCategory.ANIMALS);
+        Product product6 = new Product("fame",3,ProductCategory.OTHER);
+
+        warehouse.addNewProduct(product1);
+        warehouse.addNewProduct(product2);
+        warehouse.addNewProduct(product3);
+        warehouse.addNewProduct(product4);
+        warehouse.addNewProduct(product5);
+        warehouse.addNewProduct(product6);
+
+    }
+
 
     @Test
     public void testAddProduct(){
-
-        Product product1 = new Product("laptop",6,ProductCategory.ELECTRONICS);
-          warehouse.addNewProduct(product1);
           // check length of array
           List<Product> products = warehouse.getAllProducts();
-         assertEquals(1, products.size());
+         assertEquals(6, products.size());
 
          // check for right field
         Product addedProduct = products.get(0);
@@ -33,8 +49,6 @@ class WarehouseTest {
     }
     @Test
     public void testEditProduct(){
-        Product product = new Product("laptop",6,ProductCategory.ELECTRONICS);
-        warehouse.addNewProduct(product);
         warehouse.editProduct(1,"New laptop",9,null);
         Optional<Product> editedProduct = warehouse.getProductById(1);
         assertTrue(editedProduct.isPresent());
@@ -42,5 +56,29 @@ class WarehouseTest {
         assertEquals(9, editedProduct.get().getRating());
 
     }
+@Test
+    public void testGetAllProducts(){
+    List<Product> products = warehouse.getAllProducts();
+    assertEquals(6, products.size());
+    assertEquals("Ball", products.get(1).getName());
+   assertEquals(5,products.get(2).getRating());
 
+}
+
+@Test
+    public void testGetProductById(){
+        Optional<Product> foundProduct = warehouse.getProductById(2);
+        assertTrue(foundProduct.isPresent());
+        assertEquals("Ball", foundProduct.get().getName());
+        assertEquals(7, foundProduct.get().getRating());
+        assertEquals(ProductCategory.OTHER, foundProduct.get().getCategory());
+}
+
+@Test
+    public void testGetProductsByCategory(){
+        List<Product> electronicProducts = warehouse.getProductsByCategory(ProductCategory.ELECTRONICS);
+        assertEquals(2, electronicProducts.size());
+        assertEquals("laptop", electronicProducts.get(0).getName());
+        assertEquals("smart phone", electronicProducts.get(1).getName());
+}
 }
